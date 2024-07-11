e41f18e7be7305b7367fa05027f9182ff13a6fdc
nyxb <dennis.ollhoff@joypix.gg>
2024-07-11 19:52:10 +0200

    ✨ feat(form.tsx): add type import for LabelPrimitive from @radix-ui/react-label

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Slot } from '@radix-ui/react-slot'
+import type * as LabelPrimitive from '@radix-ui/react-label'
 import type {
    ControllerProps,
    FieldPath,
